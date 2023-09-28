8a135b3783cd9ca23ef8e3c4f05a149404b46e86
Hussein Gamal <105026648+Huss8in@users.noreply.github.com>
2023-09-28 17:21:01 +0300

    Add files via upload

---
+++ WorldCup_Ticket_Reservation/server/processors/shop.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const url = "http://localhost:5000/api/analytics/";
+
+const processPendingTicket = async (message) => {
+  const data = {
+    matchNumber: message.body.matchNumber,
+    tickets: message.body.tickets,
+    messageType: message.meta.action,
+  };
+  axios.post(url, data).then(() => {
+    console.log("done analytics!");
+  });
+
+  console.log("[processPendingTicket]", message);
+  return Promise.resolve("[processPendingTicket]");
+};
+
+const processCancelledTicket = async (message) => {
+  const data = {
+    matchNumber: message.body.matchNumber,
+    tickets: message.body.tickets,
+    messageType: message.meta.action,
+  };
+  axios.post(url, data).then(() => {
+    console.log("done analytics!");
+  });
+  console.log("[processCancelledTicket]", message);
+  return Promise.resolve("[processCancelledTicket]");
+};
+
+const processReservedTicket = async (message) => {
+  const data = {
+    matchNumber: message.body.matchNumber,
+    tickets: message.body.tickets,
+    messageType: message.meta.action,
+  };
+  axios.post(url, data).then(() => {
+    console.log("done analytics!");
+  });
+  console.log("[processReservedTicket]", message);
+  return Promise.resolve("[processReservedTicket]");
+};
+
+const processMasterlist = async (message) => {
+  console.log("[processMasterlist]", message);
+  return Promise.resolve("[processMasterlist]");
+};
+
+module.exports = {
+  processPendingTicket,
+  processReservedTicket,
+  processCancelledTicket,
+};
